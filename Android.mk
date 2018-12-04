732ab63c2fda9ebdea8522a6147a5a26ecbfcfce
Bian Jin chen <kenjc.bian@rock-chips.com>
2018-12-04 09:55:12 +0800

    liblights: fix make error for Android Pie.
    Change-Id: I6b07218a2be2c4884987639a3e428e5f25d414c2
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
